ba236fb80a7f9755d5238f384cef7f395f6a1935
Wuzup <dgr993@gmail.com>
2020-08-07 16:01:12 -0600

    tweaked the port

--- node.js
+++ node.js
@@ -1,18 +1,17 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-let password = "";
 
 var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 3000,
+  port: 3306,
 
   // Your username
   user: "root",
 
   // Your password
-  password: password,
+  password: "",
   database: "employee_tracker_db"
 });
 
